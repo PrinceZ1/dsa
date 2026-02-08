de2e3d8a12b7272c761cfd1630bc9b98d125be8c
tnngynz <nguyen.tt1703@gmail.com>
2026-02-08 22:24:32 +0700

    Sort Colors

---
+++ src/array/SortColors.java
@@ -0,0 +1,42 @@
+package array;
+
+import java.util.Arrays;
+
+public class SortColors {
+
+    /**
+     * Dutch National Flag Algorithm
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     */
+    public static void sortColors(int[] nums) {
+        int low = 0, mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else { // nums[mid] == 2
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    // Main method để test
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        sortColors(nums);
+        System.out.println(Arrays.toString(nums)); // [0, 0, 1, 1, 2, 2]
+    }
+}
